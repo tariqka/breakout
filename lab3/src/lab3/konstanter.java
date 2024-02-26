895e97b4e6191e8945f74ea6cb58640fb614e09c
tariqka <146717222+tariqka@users.noreply.github.com>
2024-02-26 15:29:11 +0100

    Add files via upload

---
+++ lab3/src/lab3/konstanter.java
@@ -0,0 +1,34 @@
+package lab3;
+
+public class konstanter {
+	public static final int fivehun = 1500;
+	public static final int threetow =320;
+	public static final int thresi =360;
+	public static final int fourse = 470;
+	public static final int twenty =20;
+	public static final int fourrhun =400;
+	public static final int threehun =300;
+	public static final int sevenhun =700;
+	public static final int fivefifty =550;
+	public static final int elevenz = 110;
+	public static final int sevennine= 799;
+	public static final int ten =10;
+	public static final int oneeighty =180;
+	public static final int towhun =200;
+	public static final int thousand =1000;
+	public static final int fifty =50;
+	public static final int fifteen =15;
+	public static final int seventy =70;
+	public static final int towfive= 25;
+	public static final int fourfive=45;
+	public static final int sixty =60;
+	public static final int ninefive =95;
+	public static final int onethree=130;
+	public static final int onesixfive=165;
+	public static final int eleven=11;
+	public static final int fiveeee=500;
+	public static final int sixhun=600;
+	public static final int eighthun =800;
+	public static final int SPEED=8;
+	
+}
